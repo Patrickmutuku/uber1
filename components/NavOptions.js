359231ecd439db0378ffdf152008ee580c9de679
patrickmutisya39@gmail.com <you@example.com>
2024-01-24 16:47:04 +0300

    third commit

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,58 +1,63 @@
-import { Text, Touchable, View, FlatList, TouchableOpacity, Image } from 'react-native'
-import React from 'react'
-import tw from 'tailwind-react-native-classnames';
-import { Icon } from 'react-native-elements';
-
+import React from "react";
+import { Text, View, FlatList, TouchableOpacity, Image } from "react-native";
+import { Icon } from "react-native-elements/dist/icons/Icon";
+import tw from "tailwind-react-native-classnames";
+import { useNavigation } from "@react-navigation/native";
+import { useSelector } from "react-redux";
+import { selectOrigin } from "../slices/navSlice";
 
 const data = [
     {
         id: "123",
         title: "Get a Ride",
-        image: 
-        "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberX.png",
+        image:
+            "https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberX.png",
         screen: "MapScreen",
     },
     {
         id: "456",
         title: "Order Food",
-        image: ("https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_485,h_385/f_auto,q_auto/products/carousel/UberX.png"),
+        image:
+            "https://i.pinimg.com/originals/4f/eb/74/4feb745209cf7aba57463b20d27b61e3.png",
         screen: "EatsScreen",
     },
 ];
 
 const NavOptions = () => {
-  return (
-    <FlatList
-    data={data}
-    horizontal
-    keyExtractor={item => item.id}
-    renderItem={({ item }) => (
-        <TouchableOpacity
-        style={
-            tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`
-        }
-        
-        >
-            <View>
-                <Image
-                style={{ width: 120, height: 120, resizeMode: "contain" }}
-                source={{ uri: item.image }}
-                />
+    const navigation = useNavigation();
+    const origin = useSelector(selectOrigin);
 
-                <Icon
-                type="antdesign"
-                color="white"
-                name="arrowright"
-                style={tw `p-2 ${origin ? "bg-black":"bg-gray-400"} rounded-full w-10 mt-4`}
-                />
-            </View>
-            <Text style={ tw `mt-2 text-lg font-semibold`}>{item.title}</Text>
-        </TouchableOpacity>
+    return (
+        <FlatList
+            data={data}
+            horizontal={true}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => {
+                return (
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate(item.screen)}
+                        style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+                        disabled={!origin}
 
-    )}
-    />
-  );
-}
+                    >
+                        <View style={tw`${!origin && 'opacity-20'}`}>
+                            <Image
+                                style={{ width: 120, height: 120, resizeMode: "contain" }}
+                                source={{ uri: item.image }}
+                            />
+                            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                            <Icon
+                                style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                                name="arrowright"
+                                type="antdesign"
+                                color="white"
+                            />
+                        </View>
+                    </TouchableOpacity>
+                );
+            }}
+        />
+    );
+};
 
-export default NavOptions
-
+export default NavOptions;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,37 +1,66 @@
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native';
-import React from 'react';
-import tw from 'tailwind-react-native-classnames';
-import NavOptions from '../components/NavOptions';
+import React from "react";
+import { SafeAreaView, View, Image } from "react-native";
+import tw from "tailwind-react-native-classnames";
+import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from '@env';
+import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlice";
+import NavFavorites from "../components/NavFavorites";
 
 const HomeScreen = () => {
-  return (
-    <SafeAreaView style={tw` bg-white h-full`}>
-<View style={tw`p-5`}>
-      <Image
-    style={{
-        width: 100,
-        height: 100,
-        resizeMode: "contain",
-    }}
-    source={{
-        uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png",
-    }}
-    />
-    <NavOptions />
-      </View>
-    </SafeAreaView>
-  );
+    const dispatch = useDispatch();
+
+    return (
+        <SafeAreaView style={tw`bg-white h-full`}>
+            <View style={tw`p-5`}>
+                <Image
+                    style={{
+                        width: 100,
+                        height: 100,
+                        resizeMode: "contain",
+                    }}
+                    source={{
+                        uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png",
+                    }}
+                />
+
+                <GooglePlacesAutocomplete
+                    placeholder="Where From?"
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
+                    styles={{
+                        container: {
+                            flex: 0,
+                        },
+                        textinput: {
+                            fontSize: 18,
+                        },
+                    }}
+                    onPress={(data, details) => {
+                        dispatch(
+                            setOrigin({
+                                location: details.geometry.location,
+                                description: data.description,
+                            })
+                        );
+                        dispatch(setDestination(null));
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    enablePoweredByContainer={false}
+                    minLength={2}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: "en",
+                    }}
+                />
+
+                <NavOptions />
+                <NavFavorites />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({
-  text: {
-    color: 'red',
-    fontSize: 30,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 50,
-    marginBottom: 50
-  },
-});
